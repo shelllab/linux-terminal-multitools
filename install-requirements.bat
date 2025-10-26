a0ffc14961c368850b7a7b1b4aa68c1dba435a67
Shell Lab <235336895+shelllab@users.noreply.github.com>
2025-10-26 19:40:34 +0100

    Create install-requirements.bat

---
+++ install-requirements.bat
@@ -0,0 +1,9 @@
+@echo off
+title Installing...                       by shelllab
+echo Installing requirements...
+pip install -r requirements.txt
+echo Requirements installed successfully!
+echo press Enter to start the linux-terminal-multitools 
+pause
+python3 main.py
+
